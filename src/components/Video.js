6b523ba61ee21a0cceefcb81fdabda60cc7be3ab
Nicole Stellatos <n_stellatos@yahoo.com>
2021-09-14 16:28:37 -0400

    added static methods for video index

--- src/components/Video.js
+++ src/components/Video.js
@@ -5,4 +5,22 @@ class Video {
 		this.constructor.all.push(this);
 		console.log(this);
 	}
+
+	static addVideo(video) {
+		new Video(video);
+	}
+
+	static renderVideoIndex() {
+		const videoContainer = document.createElement('div');
+		videoContainer.classList.add('video-container');
+		document.getElementById('main').appendChild(videoContainer);
+		this.all.forEach((video) => video.renderCard());
+	}
+
+	static getVideos() {
+		api.getVideos().then((videos) => {
+			videos.forEach((video) => Video.addVideo(video));
+			this.renderVideoIndex();
+		});
+	}
 }
