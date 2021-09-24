696c7c11109b6a1bd8779e0cea3b8926218b64a7
Nicole Stellatos <n_stellatos@yahoo.com>
2021-09-24 16:28:46 -0400

    render videos + cat on index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
-const api = new ApiService('http://localhost:3000');
-const modal = new Modal();
+const buttonDiv = () => document.getElementById('button-control');
+const button = () => document.getElementById('button');
+const ulDiv = () => document.getElementById('list');
+const ul = () => document.getElementById('videos-list');
 
-Video.getVideos();
+document.addEventListener('DOMContentLoaded', () => {
+	button().addEventListener('click', handleClick);
+});
+
+const handleClick = () => {
+	fetch('http://localhost:3000/videos')
+		.then((response) => response.json())
+		.then((json) => renderVideos(json))
+		.catch(handleError);
+};
+
+const renderVideos = (videos) => {
+	videos.forEach((element) => {
+		const li = document.createElement('li');
+		li.innerHTML = `
+        <h1 class="video-category-name">${element.category.name}</h1>
+        <h2 class="video-card">
+        <iframe src="https://www.tiktok.com/embed/${element.video_url}"   allowfullscreen scrolling="no" allow="encrypted-media;"></iframe>
+</h2>
+        `;
+		ul().appendChild(li);
+	});
+};
+
+const handleError = (error) => {
+	console.log(error);
+};
