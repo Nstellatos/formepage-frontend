bf5ba2e08463089ca67d0420eba28bfb94d6022a
Nicole Stellatos <n_stellatos@yahoo.com>
2021-09-24 18:25:53 -0400

    categories/videos/form appear on index

--- src/index.js
+++ src/index.js
@@ -1,30 +1,102 @@
 const buttonDiv = () => document.getElementById('button-control');
-const button = () => document.getElementById('button');
+const buttonShow = () => document.getElementById('button-show');
+const buttonNew = () => document.getElementById('button-new');
 const ulDiv = () => document.getElementById('list');
-const ul = () => document.getElementById('videos-list');
+const ul = () => document.getElementById('categories-list');
+const videoForm = () => document.getElementById('video-form');
 
 document.addEventListener('DOMContentLoaded', () => {
-	button().addEventListener('click', handleClick);
+	buttonShow().addEventListener('click', handleClick);
+	buttonNew().addEventListener('click', displayForm);
 });
 
-const handleClick = () => {
-	fetch('http://localhost:3000/videos')
-		.then((response) => response.json())
-		.then((json) => renderVideos(json))
-		.catch(handleError);
+const handleClick = (e) => {
+	if (ul().children.length < 1) {
+		fetch('http://localhost:3000/categories')
+			.then((response) => response.json())
+			.then((json) => renderCategories(json))
+			.catch(handleError);
+	} else {
+		ul().innerHTML = '';
+	}
 };
 
-const renderVideos = (videos) => {
-	videos.forEach((element) => {
-		const li = document.createElement('li');
-		li.innerHTML = `
-        <h1 class="video-category-name">${element.category.name}</h1>
-        <h2 class="video-card">
-        <iframe src="https://www.tiktok.com/embed/${element.video_url}"   allowfullscreen scrolling="no" allow="encrypted-media;"></iframe>
-</h2>
+const displayForm = () => {
+	if (!videoForm()) {
+		fetchCategoriesForSelect();
+		list.insertAdjacentHTML(
+			'afterend',
+			`
+        <form id="video-form">
+      <label for="video_url">Paste your link here:</label><br>
+      <input type="text" name="video_url"><br>
+      <label for="video-category">Category:</label>
+      <select id="category_id">
+      </select>
+      <input type="submit" value="add video"><br>
+      </form>
+        `,
+		);
+		videoForm().addEventListener('submit', handleSubmit);
+	} else {
+		videoForm().remove();
+	}
+};
+const fetchCategoriesForSelect = () => {
+	fetch('http://localhost:3000/categories')
+		.then((resp) => resp.json())
+		.then((json) =>
+			json.map(
+				(catObj) => `<option value="${catObj.id}"> ${catObj.name}</option>`,
+			),
+		)
+		.then(
+			(collection) =>
+				(document.querySelector('select#category_id').innerHTML =
+					collection.join(' ')),
+		);
+};
+const renderCategories = (categories) => {
+	console.log('all categories');
+	ul().innerHTML += "<h1 id='categories-header'> Categories</h1>";
+	categories.forEach((element) => renderCategory(element));
+};
+const renderCategory = (category) => {
+	const h2 = document.createElement('h2');
+	const a = document.createElement('a');
+	a.id = `category-${category.id}`;
+	a.innerText = category.name;
+	a.href = '#';
+	a.addEventListener('click', (e) => renderVideos(e, category));
+	h2.appendChild(a);
+	ul().appendChild(h2);
+};
+const renderVideos = (e, category) => {
+	const nextSibling = e.target.nextSibling;
+	if (nextSibling) {
+		const children = Array.from(e.target.parentNode.children);
+		const lis = children.slice(1);
+		lis.forEach((li) => li.remove());
+	} else {
+		category.videos.forEach((element) => renderVideo(element, category.id));
+	}
+};
+
+const renderVideo = (video, catId) => {
+	const a = document.getElementById(`category-${catId}`);
+	const li = document.createElement('li');
+	a.dataset.catId = catId;
+	li.innerHTML = `
+        <span class="video-card">
+        <iframe src="https://www.tiktok.com/embed/${video.video_url}" allowfullscreen scrolling="no" allow="encrypted-media;"></iframe>
+</span>
+<button class="delete-video" data-id="${video.id}"> Delete </button>
         `;
-		ul().appendChild(li);
-	});
+	a.parentNode.appendChild(li);
+};
+
+const handleSubmit = () => {
+	debugger;
 };
 
 const handleError = (error) => {
