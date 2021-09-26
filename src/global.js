a6ee9c0c51e7098ee655811e33fa58d26041ada9
Nicole Stellatos <n_stellatos@yahoo.com>
2021-09-25 22:02:02 -0400

    global file for elements

---
+++ src/global.js
@@ -0,0 +1,8 @@
+const buttonDiv = () => document.getElementById('button-control');
+const buttonShow = () => document.getElementById('button-show');
+const buttonNew = () => document.getElementById('button-new');
+const ulDiv = () => document.getElementById('list');
+const ul = () => document.getElementById('categories-list');
+const videoForm = () => document.getElementById('video-form');
+const videoUrl = () => document.getElementById('video_url');
+const videoCategory = () => document.getElementById('category_id');
