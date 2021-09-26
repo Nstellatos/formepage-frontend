63c9c4f4a87d1cdc2cf26a99e90ef428caccbdbc
Nicole Stellatos <n_stellatos@yahoo.com>
2021-09-25 21:43:15 -0400

    handleSubmit added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const buttonNew = () => document.getElementById('button-new');
 const ulDiv = () => document.getElementById('list');
 const ul = () => document.getElementById('categories-list');
 const videoForm = () => document.getElementById('video-form');
+const videoUrl = () => document.getElementById('video_url');
+const videoCategory = () => document.getElementById('category_id');
 
 document.addEventListener('DOMContentLoaded', () => {
 	buttonShow().addEventListener('click', handleClick);
@@ -28,13 +30,13 @@ const displayForm = () => {
 			'afterend',
 			`
         <form id="video-form">
-      <label for="video_url">Paste your link here:</label><br>
-      <input type="text" name="video_url"><br>
-      <label for="video-category">Category:</label>
-      <select id="category_id">
-      </select>
-      <input type="submit" value="add video"><br>
-      </form>
+        <label for="video_url">Paste your link here:</label><br>
+        <input type="text" name="video_url" id="video_url"><br>
+        <label for="video-category">Category:</label>
+        <select id="category_id">
+        </select>
+        <input type="submit" value="add video"><br>
+        </form>
         `,
 		);
 		videoForm().addEventListener('submit', handleSubmit);
@@ -72,8 +74,8 @@ const renderCategory = (category) => {
 	ul().appendChild(h2);
 };
 const renderVideos = (e, category) => {
-	const nextSibling = e.target.nextSibling;
-	if (nextSibling) {
+	const nextLiSibling = e.target.nextSibling;
+	if (nextLiSibling) {
 		const children = Array.from(e.target.parentNode.children);
 		const lis = children.slice(1);
 		lis.forEach((li) => li.remove());
@@ -87,16 +89,50 @@ const renderVideo = (video, catId) => {
 	const li = document.createElement('li');
 	a.dataset.catId = catId;
 	li.innerHTML = `
-        <span class="video-card">
+    <span class="video-card">
         <iframe src="https://www.tiktok.com/embed/${video.video_url}" allowfullscreen scrolling="no" allow="encrypted-media;"></iframe>
 </span>
 <button class="delete-video" data-id="${video.id}"> Delete </button>
         `;
 	a.parentNode.appendChild(li);
+	document
+		.querySelector(`button.delete-video[data-id="${video.id}"]`)
+		.addEventListener('click', handleDelete);
 };
 
-const handleSubmit = () => {
-	debugger;
+const handleSubmit = (e) => {
+	e.preventDefault();
+	const data = {
+		video_url: videoUrl().value,
+		category_id: videoCategory().value,
+	};
+	fetch('http://localhost:3000/videos', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	})
+		.then((resp) => resp.json())
+		.then((json) => handleCreateVideo(json));
+};
+const handleCreateVideo = (video) => {
+	ul().children.length < 1 ? handleClick() : renderVideo(video);
+	videoForm().reset();
+};
+
+const handleDelete = (e) => {
+	fetch(`http://localhost:3000/videos/${e.target.dataset.id}`, {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+		.then((resp) => resp.json())
+		.then((json) => {
+			e.target.parentNode.remove();
+			alert(json.message);
+		});
 };
 
 const handleError = (error) => {
