b7ebc61c2a383ebb6bc6acb51a34f9028cd647af
Nicole Stellatos <n_stellatos@yahoo.com>
2021-09-26 21:33:13 -0400

    refactored to oojs

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,10 +1,10 @@
 class Category {
 	static all = [];
+	static dropDownOptions = [];
 
 	constructor({ name, id, videos = [] }) {
 		this.name = name;
 		this.id = id;
-		this.videos = videos;
 		Category.all.push(this);
 	}
 
@@ -25,24 +25,37 @@ class Category {
 	static findOrCreateBy(categoryObj) {
 		return this.findByName(categoryObj.name) || new Category(categoryObj);
 	}
-
-	static render() {
-		ul().innerHTML += "<h1 id='categories-header'>Categories</h1>";
-		this.all.forEach((cat) => this.renderCategory(cat));
+	getVideos() {
+		return Video.all.filter((video) => this.id === video.category_id);
+	}
+	addToDropDown() {
+		const option = document.createElement('option');
+		option.value = this.id;
+		option.innerText = this.name;
+		videoCategory().append(option);
 	}
 
-	static renderCategory(category) {
-		debugger;
+	render() {
 		const h4 = document.createElement('h4');
 		const a = document.createElement('a');
-		a.id = `category-${category.id}`;
-		a.innerText = category.name;
+		a.id = `category-${this.id}`;
+		a.innerText = this.name;
 		a.href = '#';
-		a.addEventListener('click', (e) => rendervVideos(e, category));
+		a.addEventListener('click', this.renderVideos);
 		h4.appendChild(a);
-		ul().appendChild(h4);
+		categoriesSection().appendChild(h4);
 	}
 
+	renderVideos = (e) => {
+		const nextLiSibling = e.target.nextSibling;
+		if (nextLiSibling) {
+			const children = Array.from(e.target.parentNode.children);
+			const lis = children.slice(1);
+			lis.forEach((li) => li.remove());
+		} else {
+			this.getVideos().forEach((element) => element.render());
+		}
+	};
 	renderVideos(cat) {
 		return cat;
 	}
--- src/components/Video.js
+++ src/components/Video.js
@@ -6,4 +6,32 @@ class Video {
 		this.id = id;
 		Video.all.push(this);
 	}
+	static findByVideoUrl(video_url) {
+		return this.all.find(function (video) {
+			video.video_url === video_url;
+		});
+	}
+	static findById(id) {
+		return this.all.find((video) => video.id === id);
+	}
+	static findOrCreateBy(videoObj) {
+		return this.findByVideoUrl(videoObj.video_url) || new Video(videoObj);
+	}
+	render() {
+		let catSet = document.querySelector(`#category-${this.category_id}`);
+		const li = document.createElement('li');
+		catSet.dataset.catId = this.category_id;
+		li.innerHTML = `
+       
+        <iframe src="https://www.tiktok.com/embed/${this.video_url}"   allowfullscreen scrolling="no" allow="encrypted-media;"></iframe>
+<br>
+<button class="delete-video" data-id="${this.id}"> Delete</button>
+
+        
+        `;
+		catSet.parentNode.appendChild(li);
+		document
+			.querySelector(`button.delete-video[data-id='${this.id}']`)
+			.addEventListener('click', VideoService.handleDelete);
+	}
 }
