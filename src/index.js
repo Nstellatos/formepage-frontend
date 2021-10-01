6619f44ca334c3be62a652411fb8a89e5b293061
Nicole Stellatos <n_stellatos@yahoo.com>
2021-09-30 21:59:11 -0400

    added event listener for category form

--- src/index.js
+++ src/index.js
@@ -2,38 +2,5 @@ document.addEventListener('DOMContentLoaded', () => {
 	CategoryService.fetchCategories();
 	VideoService.fetchVideos();
 	videoForm().addEventListener('submit', VideoService.handleSubmit);
+	categoryForm().addEventListener('submit', CategoryService.handleSubmit);
 });
-
-// const handleClick = (e) => {
-// 	if (ul().children.length < 1) {
-// 		CategoryService.fetchCategories();
-// 	} else {
-// 		ul().innerHTML = '';
-// 	}
-// };
-
-/*
-
-const displayForm = () => {
-	if (!videoForm()) {
-		fetchCategoriesForSelect();
-		list.insertAdjacentHTML(
-			'afterend',
-			`
-        <form id="video-form">
-        <label for="video_url">Paste your link here:</label><br>
-        <input type="text" name="video_url" id="video_url"><br>
-        <label for="video-category">Category:</label>
-        <select id="category_id">
-        </select>
-        <input type="submit" value="add video"><br>
-        </form>
-        `,
-		);
-		videoForm().addEventListener('submit', VideoService.handleSubmit);
-	} else {
-		videoForm().remove();
-	}
-};
-
-*/
