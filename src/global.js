260692d447a76c9d62ba6d0a9e66dddaa3915ba4
Nicole Stellatos <n_stellatos@yahoo.com>
2021-09-30 21:58:07 -0400

    add  global id for creating cat

--- src/global.js
+++ src/global.js
@@ -7,4 +7,8 @@ const ul = () => document.getElementById('categories-list');
 const videoForm = () => document.getElementById('video-form');
 const videoUrl = () => document.getElementById('video_url');
 const videoCategory = () => document.getElementById('category_id');
+const videoIndex = () => document.getElementById('video-index');
 const categoriesSection = () => document.querySelector('#categories-section');
+const renderShow = () => document.getElementById('render-show');
+const catName = () => document.getElementById('name');
+const categoryForm = () => document.getElementById('category-form');
