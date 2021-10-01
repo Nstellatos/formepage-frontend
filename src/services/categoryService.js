1aace23d33fb4ac64e2676a3f4971f5c3ca9f82a
Nicole Stellatos <n_stellatos@yahoo.com>
2021-09-30 22:02:00 -0400

    ability to create a category

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -13,6 +13,29 @@ class CategoryService {
 			)
 			.catch(this.handleError);
 	}
+	static handleSubmit(e) {
+		e.preventDefault();
+		const data = {
+			name: catName().value,
+			category_id: videoCategory().value,
+		};
+		fetch(CategoryService.baseUrl, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		})
+			.then((resp) => resp.json())
+			.then((json) => {
+				let cat = new Category(json);
+
+				categoryForm().reset();
+				cat.addToDropDown();
+
+				cat.render();
+			});
+	}
 
 	static handleError(error) {
 		console.log(error);
